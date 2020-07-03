923f8af5082126051ceb9e018de2737af47ca6ed
d-mckee <devon.l.mckee@gmail.com>
2020-07-02 18:40:44 -0700

    CSS Change, fixing resize bar font color

--- index.js
+++ index.js
@@ -26,28 +26,38 @@ exports.decorateConfig = config =>
     borderColor: "#F9FAE3",
     colors,
 	css: `
-		${config.css || ''}
-	  	* {
-			text-rendering: optimizeLegibility;
-		}
-		.tab_tab {
-			border-color: rgba(102, 94, 75, 0.7);
-			background-color: #EAEBD5;
-			color: #665E4B;
-		}
-	  	.tab_tab.tab_active {
-			background-color: #F9FAE3;
-			color: #665E4B;
-			font-weight: bold;
-		}
-		.header_appTitle {
-			color: #665E4B;
-		}
-	  	.header_shape {
-			color: #665E4B;
-		}
-	  	.splitpane_divider {
-			background-color: rgba(102, 94, 75, 0.7);
-		}
-	`
+			${config.css || ''}
+			.tab_tab {
+				border-color: rgba(102, 94, 75, 0.7);
+				background-color: #EAEBD5;
+				color: #665E4B;
+			}
+	  		.tab_tab.tab_active {
+					background-color: #F9FAE3;
+					color: #665E4B;
+					font-weight: bold;
+			}
+			.header_appTitle {
+				color: #665E4B;
+			}
+	  		.header_shape {
+				color: #665E4B;
+			}
+	  		.splitpane_divider {
+				background-color: rgba(102, 94, 75, 0.7);
+			}
+			.header {
+				color: #665E4B;
+			}
+			.tabs_title {
+				color: #665E4B;
+			}
+			.tab_icon {
+				color: #665E4B;
+			}
+			.notifications_view * {
+				color: #665E4B;
+			}
+
+		`
   });
